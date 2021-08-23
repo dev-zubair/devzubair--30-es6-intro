a0e2312365e803e22450bafbaf3393fad91b55ef
zubairhmd <zubairhmd84@gmail.com>
2021-08-23 15:23:33 +0600

    coded 8-spread.js

---
+++ 8-spread.js
@@ -0,0 +1,19 @@
+const numbers = [10, 20, 30, 50, 60, 80];
+// console.log(numbers);
+// console.log(...numbers);
+
+//Find the big number
+const max = Math.max(10, 50, 1001);
+// const maxInArray = Math.max(numbers);
+const maxInArray = Math.max(...numbers);
+// console.log(max, maxInArray);
+
+//Find the Small number
+const min = Math.min(10, 50, 1001);
+// console.log(min);
+
+
+const numbers2 = [...numbers, 88];
+numbers.push(55);
+console.log(numbers);
+console.log(numbers2);
